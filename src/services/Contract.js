27d67ae744aa8865f9bdec94bcc6952878af2278
Suraj Singla <suraj.singla@mckinleyrice.co>
2022-10-02 15:31:47 +0530

    Update evmos graphUrl

--- src/services/Contract.js
+++ src/services/Contract.js
@@ -48,7 +48,7 @@ import { TYPE_APK } from '../constant/constant'
 export const graphQuery = async (appName = "") => {
 
     const API_URL = "https://api.thegraph.com/subgraphs/name/surajsingla333/de-store-graph";
-    const API_URL_2 = "https://eafa-103-216-143-139.ngrok.io/subgraphs/name/destore/backend"
+    const API_URL_2 = "https://0331-103-16-30-132.ngrok.io/subgraphs/name/destore/backend"
 
 
     const tokensQuery = appName === "" ? `
